5fc909458b56388fecfa2a342e5e294c2fdb34e5
wujunxiao <1226158284@qq.com>
2022-05-21 20:47:04 +0800

    添加js脚本中的del函数

--- web/js/index.js
+++ web/js/index.js
@@ -1,5 +1,9 @@
+function delfruit(fid) {
+    if (confirm("是否确认删除吗")) {
+        window.location.href = 'del.do?fid=' + fid;
+    }
+}
 
-
-function page(){
+function page() {
     window.location.href = '111?pageNo=' + pageNo;
 }
